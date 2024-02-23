e9d5565d8b9cc1bf8fee3a57e81e336b84f49235
Ahmad <akrt2020@gmail.com>
2024-02-23 18:14:48 +0300

    added buttons to each page

--- lib/main_screens/Make_a_group_page/make_a_group.dart
+++ lib/main_screens/Make_a_group_page/make_a_group.dart
@@ -7,10 +7,11 @@ class MakeAGroup extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title:Text("Make a group"),
+        title:Text("Make a group", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),),
         backgroundColor: Colors.red,
         elevation: 0,
-        leading: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios_new),),
+        centerTitle: true,
+        leading: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios_new), color: Colors.white,),
 
       ),
       body: Column(
--- lib/main_screens/home_page/home.dart
+++ lib/main_screens/home_page/home.dart
@@ -1,24 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:real/main_screens/Make_a_group_page/make_a_group.dart';
+import 'package:real/main_screens/check_out_page/checkout.dart';
 import 'package:real/main_screens/food_item_page/FoodItem.dart';
+import 'package:real/main_screens/full_menu_page/full_menu_page.dart';
+import 'package:real/main_screens/order_complete_page/order_complete.dart';
+import 'package:real/main_screens/personal_cart_page/personal_cart_page.dart';
+import 'package:real/main_screens/resturant_page/resturant.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  GestureDetector(
-      onTap: () {
+    return  Column(
+      children: [
+
+        SizedBox(
+          height: 200,
+        ),
+        ElevatedButton(
+        onPressed: () {
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => MakeAGroup()),
         );
       },
-      child: Container(
-        color: Colors.red,
-        width: 50,
-        height: 50,
-      ),
+        child: const Text("Make a group page")),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Checkout()),
+              );
+            },
+            child: const Text("checkout page")),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FoodItem()),
+              );
+            },
+            child: const Text("Food Items page")),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FullMenuPage()),
+              );
+            },
+            child: const Text("Full menu page")),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => OrderComplete()),
+              );
+            },
+            child: const Text("Order complete page")),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PersonalCart()),
+              );
+            },
+            child: const Text("Personal cart page")),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Resturant()),
+              );
+            },
+            child: const Text("Resturant page")),
+
+      ]
     );
   }
 }
